fe2c874ec827bc681d40046528542a13e6feefd1
reemaprajapati-scu <r.prajapati.11@student.scu.edu.au>
2023-09-18 21:00:35 +1000

    initialise project

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * Entry point of project
+ *
+ * @author (reema)
+ * @version (18/09/2023)
+ */
+public class Main
+{
+   public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
